4b55ddfef8fcc628ee44ab18f315fb895eff4dae
Pavel Korshunau <p.korshunov@section228.com>
2023-08-06 21:33:50 +0300

    add visible animation for project item

--- src/app/projects/ui/project/project.component.ts
+++ src/app/projects/ui/project/project.component.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component, Input, signal } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  inject,
+  signal,
+} from '@angular/core'
 import { Project } from '@app/projects/models'
 import { Technology, TechnologyItem, technologies } from '@app/shared'
 
@@ -12,13 +20,26 @@ import { Technology, TechnologyItem, technologies } from '@app/shared'
   imports: [CommonModule, TechnologyItem],
   host: {
     '[attr.data-reversed]': 'isReversed()',
+    '[attr.data-visible]': 'isVisible()',
   },
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
+  public readonly elementRef = inject(ElementRef)
+
   public readonly project = signal<Project | null>(null)
   public readonly isReversed = signal<boolean>(false)
-
   public readonly isImageLoaded = signal<boolean>(false)
+  public readonly isVisible = signal<boolean>(false)
+
+  public readonly intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.isVisible.set(true)
+      } else {
+        this.isVisible.set(false)
+      }
+    })
+  })
 
   @Input({ alias: 'project', required: true })
   public set projectSetter(value: Project) {
@@ -28,6 +49,10 @@ export class ProjectComponent {
   @Input({ alias: 'reversed', required: true })
   public set resersedSetter(value: boolean) {
     this.isReversed.set(value)
+  }
+
+  public ngOnInit(): void {
+    this.intersectionObserver.observe(this.elementRef.nativeElement)
   }
 
   public findTechnology(id: Technology['id']): Technology | undefined {
