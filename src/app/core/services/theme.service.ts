da91312fe407526e2559c30f881a00a1af13b14c
Pavel Korshunau <p.korshunov@section228.com>
2023-09-10 22:25:18 +0300

    add changing theme

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, inject, signal } from '@angular/core'
+import { DOCUMENT } from '@angular/common'
+import { Injectable, effect, inject, signal } from '@angular/core'
 import { LocalStorageKey, Theme } from '@app/core'
 import { LocalStorageService } from './local-storage.service'
 
@@ -7,15 +8,51 @@ import { LocalStorageService } from './local-storage.service'
 })
 export class ThemeService {
   private readonly localStorageService = inject(LocalStorageService)
+  private readonly document = inject(DOCUMENT)
+
+  constructor() {
+    this.initThemeLinkElement()
+  }
 
   public readonly theme = signal<Theme>(this.get())
+  private readonly themeLinkElement = signal<HTMLLinkElement | null>(null)
+
+  public readonly applyTheme = effect(
+    () => {
+      this.themeLinkElement.update(element => {
+        if (element) {
+          element.href = `${this.theme()}.css`
+          return element
+        }
+        return null
+      })
+    },
+    { allowSignalWrites: true },
+  )
 
   public get(): Theme {
-    return (this.localStorageService.getItem(LocalStorageKey.Theme) as Theme) ?? Theme.Dark
+    return (this.localStorageService.getItem(LocalStorageKey.Theme) as Theme) ??
+      this.isDarkColorSchemePrefered()
+      ? Theme.Dark
+      : Theme.Light
   }
 
   public toggle(): void {
     this.theme.update(theme => (theme === Theme.Dark ? Theme.Light : Theme.Dark))
     this.localStorageService.setItem(LocalStorageKey.Theme, this.theme())
+  }
+
+  private initThemeLinkElement(): void {
+    const headElement = this.document.getElementsByTagName('head')[0]
+    const themeLinkElement = this.document.createElement('link')
+
+    themeLinkElement.rel = 'stylesheet'
+
+    headElement.appendChild(themeLinkElement)
+    this.themeLinkElement.set(themeLinkElement)
+  }
+
+  private isDarkColorSchemePrefered(): boolean {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
   }
 }
